93f371e4ea3a3c9dcbcdf25d7b474e0653bc3207
Vlad Volkov <vlad@hiberbee.com>
2020-07-16 19:23:16 +0300

    Updated with Dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# github-action-minikube
+# Github Action Minikube
--- test/minikube.test.ts
+++
@@ -1,7 +0,0 @@
-import { getDownloadUrl } from '../src/minikube'
-
-test('Minikube download', async () => {
-  const version = '1.12.0'
-  const url = getDownloadUrl('1.12.0')
-  expect(url).toContain(version)
-})
