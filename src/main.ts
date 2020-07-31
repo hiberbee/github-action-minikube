5ced994c2ec8e2f6b26d147bbca96be53ea8c932
Vlad Volkov <vlad@hiberbee.com>
2020-07-31 09:29:04 +0300

    Ip output & Docker env population

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
-import { getInput, setFailed, exportVariable } from '@actions/core'
-
+import { getInput, setFailed, exportVariable, setOutput, error, info } from '@actions/core'
 import minikubeUrl, { start } from 'src/minikube'
 import download from 'src/download'
 import kubectlUrl from 'src/kubectl'
+import { exec } from '@actions/exec'
+import { ExecOptions } from '@actions/exec/lib/interfaces'
 
 async function run(): Promise<void> {
+  const options: ExecOptions = {}
+  const profile = getInput('profile')
+  exportVariable('MINIKUBE_PROFILE_NAME', profile)
+
   try {
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        const ip = data.toString()
+        exportVariable("DOCKER_HOST", `tcp://${ip}:2376`)
+        exportVariable("DOCKER_TLS_VERIFY", "1")
+        exportVariable("DOCKER_CERT_PATH", `${process.env.HOME ?? '~'}/.minikube/certs`)
+        exportVariable("MINIKUBE_ACTIVE_DOCKERD", profile)
+        setOutput('ip', data.toString())
+      },
+      stderr: (data: Buffer) => {
+        error(data.toString())
+      },
+    }
     await download({
       url: minikubeUrl(getInput('version')),
       dir: '/home/runner/bin',
@@ -16,17 +34,19 @@ async function run(): Promise<void> {
       dir: '/home/runner/bin',
       file: 'kubectl',
     })
-    exportVariable('MINIKUBE_PROFILE_NAME', getInput('profile'))
     await start({
       nodes: Number.parseInt(getInput('nodes')),
       addons: getInput('addons').split(','),
       cpus: Number.parseInt(getInput('cpus')),
       kubernetesVersion: getInput('kubernetes-version'),
       networkPlugin: getInput('network-plugin'),
-    })
+    }).then(() => exec('minikube', ['ip'], options))
+
   } catch (error) {
     setFailed(error.message)
   }
 }
 
-run().then(() => console.log('Minikube cluster is ready'))
+run().then(() => {
+  info('Minikube cluster is ready')
+})
